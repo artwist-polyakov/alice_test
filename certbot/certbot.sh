ecc96f10522be843df9d5e058a1f13fe68adfda6
Aleksandr Poliakov <master@artwist.ru>
2024-05-16 21:00:08 +0300

    меняет конфигурацию для получения сертификатов

--- certbot/certbot.sh
+++ certbot/certbot.sh
@@ -3,5 +3,10 @@
 # Получение SSL-сертификатов
 certbot certonly --webroot --webroot-path=/var/www/certbot --email artwist@yandex.ru --agree-tos --no-eff-email -d practix-cinema.ru -d www.practix-cinema.ru
 
+# Копирование ssl конфигурации в директории  nginx
+cp /etc/nginx/support/practix-cinema.ru.ssl.conf /etc/nginx/conf.d/
+
+docker-compose restart nginx
+
 # Обновление сертификатов каждые 12 часов
 trap exit TERM; while :; do certbot renew; sleep 12h & wait ${!}; done;
