f4808f4d1287ef0c530faa0546b1e6e02d96bf35
Aleksandr Poliakov <master@artwist.ru>
2024-05-16 19:34:43 +0300

    дорабатывает конфигурацию

--- certbot/certbot.sh
+++ certbot/certbot.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-certbot certonly --webroot --webroot-path=/var/www/certbot --email your-email@example.com --agree-tos --no-eff-email -d practix-cinema.ru -d www.practix-cinema.ru
+certbot certonly --webroot --webroot-path=/var/www/certbot --email artwist@yandex.ru --agree-tos --no-eff-email -d practix-cinema.ru -d www.practix-cinema.ru
 trap exit TERM; while :; do certbot renew; sleep 12h & wait $${!}; done;
