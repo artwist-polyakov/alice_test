d54327df84acf36cbccced0f69545c62487f3012
Aleksandr Poliakov <master@artwist.ru>
2024-05-16 21:57:15 +0300

    меняет команду перезагрузки контейнера

--- certbot/certbot.sh
+++ certbot/certbot.sh
@@ -8,7 +8,7 @@ certbot certonly --webroot --webroot-path=/var/www/certbot --email artwist@yande
 # Копирование ssl конфигурации в директории  nginx
 cp /etc/nginx/support/practix-cinema.ru.ssl /etc/nginx/conf.d/practix-cinema.ru.ssl.conf
 
-docker-compose restart nginx
+docker restart nginx
 
 # Обновление сертификатов каждые 12 часов
 trap exit TERM; while :; do certbot renew; sleep 12h & wait ${!}; done;
