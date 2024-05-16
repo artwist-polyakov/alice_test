c798866d3021b411a6a907cb6131eda832340736
Aleksandr Poliakov <master@artwist.ru>
2024-05-16 20:13:26 +0300

    обновляет настройки

--- certbot/certbot.sh
+++ certbot/certbot.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Получение SSL-сертификатов
-certbot certonly --webroot --webroot-path=/var/www/certbot --artwist@yandex.ru --agree-tos --no-eff-email -d practix-cinema.ru -d www.practix-cinema.ru
+certbot certonly --webroot --webroot-path=/var/www/certbot --email artwist@yandex.ru --agree-tos --no-eff-email -d practix-cinema.ru -d www.practix-cinema.ru
 
 # Обновление сертификатов каждые 12 часов
 trap exit TERM; while :; do certbot renew; sleep 12h & wait ${!}; done;
