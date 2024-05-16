b69ff208aa4efad77d857911c9c60a021a087612
Aleksandr Poliakov <master@artwist.ru>
2024-05-17 00:25:32 +0300

    новый синтаксиси

--- nginx/reload.js
+++ nginx/reload.js
@@ -1,6 +1,8 @@
-export function reloadNginx(r) {
+function reloadNginx(r) {
     r.return(200, "Reloading Nginx...");
 
     // Используем exec для выполнения команды перезагрузки
     r.variables.exec("/usr/sbin/nginx -s reload");
 }
+
+module.exports = {reloadNginx};
