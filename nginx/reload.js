3d4556522b81438f49a63e7bbf2c0669c1c83be7
Aleksandr Poliakov <master@artwist.ru>
2024-05-16 23:35:10 +0300

    еще одна попытка

--- nginx/reload.js
+++ nginx/reload.js
@@ -1,8 +1,5 @@
 function reloadNginx(r) {
-    var sys = require('sys');
-    sys.exec('/usr/sbin/nginx -s reload', function(stdout, stderr) {
-        r.return(200, "Nginx reloaded successfully");
+    r.subrequest('/internal/reload', {}, function(reply) {
+        r.return(reply.status, reply.responseBody);
     });
 }
-
-export default {reloadNginx};
